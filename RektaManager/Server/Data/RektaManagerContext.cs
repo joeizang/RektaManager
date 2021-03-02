6c49d0b17661304956bc4fac83bc1559b6a06460
Joe Izang <joeizang@hotmail.com>
2021-03-03 00:22:48 +0100

    Add project files.

---
+++ RektaManager/Server/Data/RektaManagerContext.cs
@@ -0,0 +1,17 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using RektaManager.Shared;
+
+namespace RektaManager.Server.Data
+{
+    public class RektaManagerContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public RektaManagerContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
